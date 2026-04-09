1efc8d24cfced8d9bd576571cb3970681bb4bacf
gasu <sbrofati.alessandro@gmail.com>
2026-04-09 05:45:08 +0200

    Añadiendo el proyectoi al repositorio

---
+++ academiaespacial/src/main/java/es/fpsumma/academiaespacial/AcademiaespacialApplication.java
@@ -0,0 +1,13 @@
+package es.fpsumma.academiaespacial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaespacialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaespacialApplication.class, args);
+	}
+
+}
---
+++ academiaespacial/src/test/java/es/fpsumma/academiaespacial/AcademiaespacialApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fpsumma.academiaespacial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaespacialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
